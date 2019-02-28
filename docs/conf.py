fbb7c6207af929cc8075acbd26d305e200b49afc
Tyler Kennedy <tk@tkte.ch>
2019-02-28 04:19:44 -0500

    Bump version: 1.4.0 → 1.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Tyler Kennedy'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.4.0'
+release = '1.4.1'
 
 
 # -- General configuration ---------------------------------------------------
