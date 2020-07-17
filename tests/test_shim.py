5729f29d5538424e7e0fc0363d40c1b39833ee78
Tyler Kennedy <tk@tkte.ch>
2020-07-17 14:25:53 -0400

    fix shim test on <= py3.5

--- tests/test_shim.py
+++ tests/test_shim.py
@@ -4,7 +4,9 @@ import simdjson
 
 def test_load():
     """Ensure basic usage of load is the same."""
-    with open('jsonexamples/canada.json', 'rb') as fin:
+    # We don't use a binary file here because pre-py3.6 the built-in couldn't
+    # handle bytes.
+    with open('jsonexamples/canada.json', 'r') as fin:
         builtin_json = json.load(fin)
 
     with open('jsonexamples/canada.json', 'rb') as fin:
