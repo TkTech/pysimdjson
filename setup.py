3f485370b2a76befd473ef91c215da4b67c0d47d
Tyler Kennedy <tk@tkte.ch>
2022-05-29 06:35:02 -0400

    Bump version: 4.0.3 → 5.0.0

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ else:
 setup(
     name='pysimdjson',
     packages=find_packages(),
-    version='4.0.3',
+    version='5.0.0',
     description='simdjson bindings for python',
     long_description=long_description,
     long_description_content_type='text/markdown',
