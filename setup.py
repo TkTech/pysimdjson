3080f2c3f6c0e3317f55742c502eabbaaa855d86
Shuyang Wang <4240939+harrycis@users.noreply.github.com>
2022-09-01 21:48:53 -0700

    Update simdjson to 2.0.3

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ else:
 setup(
     name='pysimdjson',
     packages=find_packages(),
-    version='5.0.1',
+    version='5.0.2',
     description='simdjson bindings for python',
     long_description=long_description,
     long_description_content_type='text/markdown',
